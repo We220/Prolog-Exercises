b40e958cf90e788ff0d77a20b2d21c425d4c1c57
We220 <we220@bath.ac.uk>
2016-02-03 17:11:28 +0000

    Completed Prolog Problems 1 and 2

---
+++ PP2.pl
@@ -0,0 +1,10 @@
+#Find the last element but one element of the list (Find the second last element of a list)
+
+my_last_by_one(X, [X,_]).
+my_last_by_one(X,[_,T|Ts]) :- my_last_by_one(X, [T|Ts]).
+
+
+#Find the third last element
+
+my_last_by_two(X, [X,_]).
+my_last_by_two(X,[_,T|Ts]) :- my_last_by_two(X, [T|Ts]).
